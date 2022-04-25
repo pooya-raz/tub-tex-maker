3e26658be456baa81f16327174f38d7438c77447
Christopher Pooya Razavian <pooya72@gmail.com>
2022-04-26 01:30:46 +0430

    Remove test code.

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ int main() {
     auto result =  tub.getQuery("[[Category:Title]]|?Title%20(Arabic)|?Title%20(transliterated)|?Has%20author(s)|?Has%20author(s).Death%20(Hijri)|?Has%20author(s).Death%20(Gregorian)|?Has%20author(s).Death%20(Hijri)%20text|?Has%20author(s).Death%20(Gregorian)%20text|?Has%20a%20catalogue%20description|limit=5");
     fileWriter.save_to_file(result);
 
-    std::cout << "Hello, ф or \u0444!\n";
     return 0;
 
 }
