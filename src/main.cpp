74c5f51b989860ee2ddfeb91f17d1f262fd95058
Christopher Pooya Razavian <pooya72@gmail.com>
2022-05-09 16:09:44 +0430

    Fix proper variables so it reads correct manuscript json.

--- src/main.cpp
+++ src/main.cpp
@@ -52,11 +52,13 @@ int main() {
             "|?Manuscript%20number"
             "|?Manuscript%20of%20title"
             "|limit=1000"
+            "|offset=0"
             );
     fileWriter.save_to_file("response-manuscript.json", manuscripts);
     TubJson manuJson;
     manuJson.parse(manuscripts);
     entryManager.add_manuscripts(manuJson);
+    BOOST_LOG_TRIVIAL(info) << "The manuscript2 offset: " << manuJson.get_int("query-continue-offset");
 
     auto manuscripts2 = tub.getQuery(
             "[[Category:Manuscript]]"
@@ -76,7 +78,7 @@ int main() {
     TubJson manuJson2;
     manuJson2.parse(manuscripts2);
     entryManager.add_manuscripts(manuJson2);
-    BOOST_LOG_TRIVIAL(info) << "The offset: " << manuJson2.get_int("query-continue-offset");
+    BOOST_LOG_TRIVIAL(info) << "The manuscript2 offset: " << manuJson2.get_int("query-continue-offset");
 
     auto manuscripts3 = tub.getQuery(
             "[[Category:Manuscript]]"
@@ -90,13 +92,13 @@ int main() {
             "|?Manuscript%20number"
             "|?Manuscript%20of%20title"
             "|limit=1000"
-            "|offset=1000"
+            "|offset=2000"
     );
-    fileWriter.save_to_file("response-manuscript2.json", manuscripts3);
+    fileWriter.save_to_file("response-manuscript3.json", manuscripts3);
     TubJson manuJson3;
-    manuJson2.parse(manuscripts3);
+    manuJson3.parse(manuscripts3);
     entryManager.add_manuscripts(manuJson3);
-    BOOST_LOG_TRIVIAL(info) << "The offset: " << manuJson3.get_int("query-continue-offset");
+    BOOST_LOG_TRIVIAL(info) << "The manuscript3 offset: " << manuJson3.get_int("query-continue-offset");
 
     auto editions = tub.getQuery(
             "[[Category:Edition]]"
@@ -125,6 +127,11 @@ int main() {
      * Add commentaries
      */
     entryManager.add_commentaries();
+    for(const auto& entry: entryManager.getEntries()){
+        if(entry->getId() == "Ḥāshiyat ʿUddat al-uṣūl - al-Ḥāshiyat al-ūlā"){
+            BOOST_LOG_TRIVIAL(info) << "Entry found " << entry->getId() << "\n title type: " << entry->getTitleType();
+        }
+    }
 
     BOOST_LOG_TRIVIAL(info) << "Total number of entries: " << entryManager.getEntries().size();
 
