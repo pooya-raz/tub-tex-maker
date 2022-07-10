d1fbe9d2231072a375da51909bdf5b2b3513d991
pooya-raz <pooya72@gmail.com>
2022-07-10 19:36:22 +0100

    [#13] setup testing for latex method in models (#14)
    * Add testing for editions.
    
    * Add testing for authors.
    
    * Add rest of tests
    
    Co-authored-by: Chris Razavian <christopherrazavian@hollandandbarrett.com>

---
+++ src/models/edition_test.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Christopher Razavian on 10/07/2022.
+//
+#include <gtest/gtest.h>
+#include "edition.h"
+
+TEST(EditionTestAllFields, BasicTest) {
+
+    Edition edition = Edition("Title Transliterated",
+                              "Title Arabic",
+                              "Editor",
+                              "Lithograph",
+                              "Publisher",
+                              "City",
+                              700,
+                              1300, "NO DATA", "NO DATA", "", "Published edition of title");
+
+    auto expected = "\\item \\emph{Title Transliterated}, Editor, Lithograph, Publisher, City, 700/1300\n";
+    EXPECT_EQ(expected, edition.to_latex());
+}
---
+++ src/models/manuscript_test.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Christopher Razavian on 10/07/2022.
+//
+#include <gtest/gtest.h>
+#include "manuscript.h"
+
+TEST(ManuscriptTestAllFields, BasicTest) {
+
+    Manuscript manuscript = Manuscript(
+            "Location",
+            700,
+            1300,
+            "NO DATA",
+            "NO DATA",
+            "City",
+            "Manuscript Number",
+            "Manuscript of Title"
+    );
+
+    auto expected = "\\item Location, City (\\#Manuscript Number), dated 700/1300\n";
+    EXPECT_EQ(expected, manuscript.to_latex());
+}
