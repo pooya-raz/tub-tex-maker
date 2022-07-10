b66f9109c7cfb3d85d12ac7e90924c94118f08bf
Chris Razavian <christopherrazavian@hollandandbarrett.com>
2022-07-10 20:47:04 +0100

    Make headers bold

--- src/formatters/latex_formatter.cpp
+++ src/formatters/latex_formatter.cpp
@@ -5,33 +5,30 @@
 #include "latex_formatter.h"
 
 
+std::string latex_formatter::to_latex(const EntryMap &entryMap) {
+    auto build_section{
+            [](TitleType title_type, const std::string &label, const EntryMap &map) {
+                if (map.contains(title_type)) {
+                    auto entriesv = map.at(title_type);
 
-std::string latex_formatter::to_latex(const EntryMap& entryMap) {
-    auto build_section {
-        [](TitleType title_type, const std::string& label, const EntryMap& map){
-            if(map.contains(title_type))
-            {
-                auto entriesv = map.at(title_type);
-
-                std::string output = fmt::format("\\section{{{}}}\n\\begin{{enumerate}}\n",label);
-                auto latex_fold = [](
-                        std::string a,
-                        const std::shared_ptr<Entry>& b) {
-                    return std::move(a) + b->to_latex();
-                };
-                auto entries = std::accumulate(
-                        std::next(entriesv.begin()),
-                        entriesv.end(),
-                        entriesv[0]->to_latex(),
-                        latex_fold);
-                output += entries + "\\end{enumerate}\n";
-                return output;
+                    std::string output = fmt::format("\\section{{{}}}\n\\begin{{enumerate}}\n", label);
+                    auto latex_fold = [](
+                            std::string a,
+                            const std::shared_ptr<Entry> &b) {
+                        return std::move(a) + b->to_latex();
+                    };
+                    auto entries = std::accumulate(
+                            std::next(entriesv.begin()),
+                            entriesv.end(),
+                            entriesv[0]->to_latex(),
+                            latex_fold);
+                    output += entries + "\\end{enumerate}\n";
+                    return output;
+                } else {
+                    std::string empty;
+                    return empty;
+                }
             }
-            else{
-                std::string empty;
-                return empty;
-            }
-        }
     };
 
     std::string final;
@@ -39,7 +36,7 @@ std::string latex_formatter::to_latex(const EntryMap& entryMap) {
              "    \\usepackage{fontspec,lipsum}\n"
              "    \\defaultfontfeatures{Ligatures=TeX}\n"
              "    \\usepackage[small,sf,bf]{titlesec}\n"
-             "    \\setromanfont{Gentium Plus}\n"
+             "    \\setromanfont[BoldFont={Gentium Basic Bold}{Gentium}"
              "    \\newfontfamily\\arabicfont[Script=Arabic]{Amiri}\n"
              "    \\usepackage{polyglossia}\n"
              "    \\setmainlanguage{english}\n"
@@ -51,20 +48,22 @@ std::string latex_formatter::to_latex(const EntryMap& entryMap) {
              "    \\maketitle\n"
              "    \\tableofcontents\n"
              "    \\pagebreak\n";
-    std::map<TitleType,std::string> section_map {
-        {Monograph,"Monograph"},
-        {Treatise,"Treatise (risāla)"},
-        {Commentary,"Commentary (sharḥ)"},
-        {Gloss,"Gloss (ḥāshīyah)"},
-        {MarginalNotes,"Marginal notes (taʿlīqa)"},
-        {Summary,"Summary (khulāṣa/mukhtaṣar)"},
-        {Poem,"Poem (manẓūma)"},
-        {Refutation,"Refutation (radd)"},
-        {Taqrirat,"Taqrīrāt"},
-        {tCorrectionsRequired,"Corrections required in title type"},
+
+    std::map<TitleType, std::string> section_map{
+            {Monograph,            "Monograph"},
+            {Treatise,             "Treatise (risāla)"},
+            {Commentary,           "Commentary (sharḥ)"},
+            {Gloss,                "Gloss (ḥāshīyah)"},
+            {MarginalNotes,        "Marginal notes (taʿlīqa)"},
+            {Summary,              "Summary (khulāṣa/mukhtaṣar)"},
+            {Poem,                 "Poem (manẓūma)"},
+            {Refutation,           "Refutation (radd)"},
+            {Taqrirat,             "Taqrīrāt"},
+            {tCorrectionsRequired, "Corrections required in title type"},
     };
-    for (const auto& section: section_map){
-            final += build_section(section.first, section.second,entryMap);
+
+    for (const auto &section: section_map) {
+        final += build_section(section.first, section.second, entryMap);
     }
     final += "\\end{document}";
     return final;
--- src/formatters/latex_formatter.h
+++ src/formatters/latex_formatter.h
@@ -4,10 +4,11 @@
 
 #ifndef TUB_PDF_MAKER_LATEX_FORMATTER_H
 #define TUB_PDF_MAKER_LATEX_FORMATTER_H
+
 #include <string>
 #include "../managers/entry_manager.h"
-namespace latex_formatter
-{
-    std::string to_latex(const EntryMap& entryMap);
+
+namespace latex_formatter {
+    std::string to_latex(const EntryMap &entryMap);
 }
 #endif //TUB_PDF_MAKER_LATEX_FORMATTER_H
