e0742f2a931bef733d44c343708daf4ab8d3288e
Chris Razavian <christopherrazavian@hollandandbarrett.com>
2022-07-10 19:42:06 +0100

    Get rid of modern print

--- src/models/edition_test.cpp
+++ src/models/edition_test.cpp
@@ -18,3 +18,18 @@ TEST(EditionTestAllFields, BasicTest) {
     auto expected = "\\item \\emph{Title Transliterated}, Editor, Lithograph, Publisher, City, 700/1300\n";
     EXPECT_EQ(expected, edition.to_latex());
 }
+
+TEST(EditionModernPrintTest, BasicTest) {
+
+    Edition edition = Edition("Title Transliterated",
+                              "Title Arabic",
+                              "Editor",
+                              "Modern Print",
+                              "Publisher",
+                              "City",
+                              700,
+                              1300, "NO DATA", "NO DATA", "", "Published edition of title");
+
+    auto expected = "\\item \\emph{Title Transliterated}, Editor, Publisher, City, 700/1300\n";
+    EXPECT_EQ(expected, edition.to_latex());
+}
