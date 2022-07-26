166ccc5841a43ca27d1b34046f17aeb6a5ff2146
Chris Razavian <christopherrazavian@hollandandbarrett.com>
2022-07-26 09:01:56 +0100

    Remove "Editions" section for all title types that are not Edited titles
    Currently, all title types show an Edition section, even though only Edited titles will have editions. Manuscript-only and Non-extant titles do not have editions.

--- src/models/entry_test.cpp
+++ src/models/entry_test.cpp
@@ -6,7 +6,7 @@
 #include "author.h"
 #include "manuscript.h"
 
-TEST(EntryAllFields, BasicTest) {
+TEST(Entry, BasicTest) {
 
     Author author = Author(
             "Name Transliterated",
@@ -45,5 +45,47 @@ TEST(EntryAllFields, BasicTest) {
     entry.manuscripts = manuscripts;
 
     auto expected = "\\item Title Transliterated\n        \\newline\n        \\textarabic{Title Arabic}\n        \\newline\n        Name Transliterated\n        \\newline\n        (d. 8th century/14th century)\n        \\newline\n        \\newline\n        \\textbf{Principal Manuscripts}\n\\begin{itemize}\n\\item Location, City (\\#Manuscript Number), dated 700/1300\n\\end{itemize}\n        \\textbf{Editions}\n\\begin{itemize}\n\\item NO DATA\n\\end{itemize}\n        \n";
+    EXPECT_EQ(expected, entry.to_latex());
+}
+
+TEST(Entry, ManuscriptOnly) {
+
+    Author author = Author(
+            "Name Transliterated",
+            0,
+            0,
+            "8th century",
+            "14th century");
+    Manuscript manuscript = Manuscript(
+            "Location",
+            700,
+            1300,
+            0,
+            "NO DATA",
+            "NO DATA",
+            "NO DATA",
+            "City",
+            "Manuscript Number",
+            "Manuscript of Title",
+            0
+    );
+    std::vector<Manuscript> manuscripts = {manuscript};
+    std::vector<CorrectionsRequired> corrections_required = {};
+
+    Entry entry = Entry(
+            "1",
+            "Title Transliterated",
+            "Title Arabic",
+            "Description",
+            ManuscriptOnly,
+            corrections_required,
+            Monograph,
+            "Base Text",
+            "Author Title Page",
+            "");
+    entry.setAuthor(author);
+    entry.manuscripts = manuscripts;
+
+    auto expected = "\\item Title Transliterated\n        \\newline\n        \\textarabic{Title Arabic}\n        \\newline\n        Name Transliterated\n        \\newline\n        (d. 8th century/14th century)\n        \\newline\n        \\newline\n        \\textbf{Principal Manuscripts}\n\\begin{itemize}\n\\item Location, City (\\#Manuscript Number), dated 700/1300\n\\end{itemize}\n                \n";
     EXPECT_EQ(expected, entry.to_latex());
 }
